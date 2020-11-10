4b0d5997f99202d4224f31be7a5ea530f164ebfd
Cookie Vang <vang2660@morris.umn.edu>
2020-11-10 12:21:08 -0600

    Add clarifying comments about disemvowel method

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,24 @@ use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*;
 use std::process::Command; // Run programs // Used for writing assertions
 
+/*
+This method should be called with 2 command line arguments, both file names. The first should be the name of a file
+containing the text to disemvowel, and the second should be the file we want to write the disemvowled text to
+*/
 fn main() {
-    // This should be called with two command line arguments,
-    // both file names. The first should be the name of a file
-    // containing the text to disemvowel, and the second should
-    // be the file we want to write the disemvoweled text to.
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect(); //put all command line arguments into a vector
     match args.len() {
-        0 | 1 | 2 => panic!("Not enough arguments; exactly 2 arguments needed"),
-        3 => {
+        0 | 1 | 2 => panic!("Not enough arguments; exactly 2 arguments needed"), //if too few arguments are passed in, return this
+        3 => { //run this when there are exactly 2 arguments passed in
             let input_path = Path::new(&args[1]);
-            let s = String::from(read_file(&input_path));
+            let s = String::from(read_file(&input_path)); 
 
             let s_disemvowel = disemvowel(&s);
-        
-            // Use command-line arguments for the name of the file,
-            // and s_disemvowel for the text to write out.
-            write_file(Path::new(&args[2]), &s_disemvowel);
+
+            //creating a new path with the second argument and writing out the disemvoweled string
+            write_file(Path::new(&args[2]), &s_disemvowel); 
         },
-        _ => panic!("Too many arguments; exactly 2 arguments needed")
+        _ => panic!("Too many arguments; exactly 2 arguments needed") //if there are too many arguments passed in, return this
     }
 
 }
@@ -38,18 +37,22 @@ fn write_file(path: &Path, s: &str) {
     fs::write(path, s).expect("Unable to write file");
 }
 
+/*
+This method will peek at the characters of the input string. It loops through the string using Some(c) which means
+as long as there are still some characters to look at look at the next character. If the next character matches a vowel,
+nothing is done, so it's essentially skipped. _ => output.push(c) will look at any other character in the input string and
+push it to the newly created output string. The disemvoweled string is then returned
+*/
 fn disemvowel(s: &str) -> String {
     let mut input_chars = s.chars().peekable();
     let mut output = String::new();
     while let Some(c) = input_chars.next() {
         match c {
-            'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => {}
-            _ => output.push(c)
+            'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => {} 
+            _ => output.push(c) 
         }
     }  
-
-
-    String::from(output)
+    String::from(output) 
 }
 
 // Everything from here down is Rust test code. You shouldn't need to
@@ -116,16 +119,18 @@ mod tests {
     // Tests that check that the correct panics are generated when
     // there aren't the correct number of command line arguments
     // or the input file isn't readable.
+    // Needed to fix the cargo_bin name to "disemvowel-in-rust"
     mod panic_tests {
         use super::*;
         #[test]
         fn requires_two_arguments_under() {
-            let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
+            let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap(); 
             cmd.arg("1");
             cmd.assert()
                 .failure()
                 .stderr(predicate::str::contains("Not enough arguments; exactly 2 arguments needed"));
         }
+        //adding this test to specify the condition when there are too many arguments
         #[test]
         fn requires_two_arguments_over() {
             let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
